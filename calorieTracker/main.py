e1deb2667e8b70327b56556c278017d9ca0ac62a
Andrew Pearson Caldwell <espio301@Andrews-MacBook-Air.local>
2025-12-20 23:18:46 -0800

    very basic functionality for calorie tracker

--- calorieTracker/main.py
+++ calorieTracker/main.py
@@ -12,24 +12,54 @@
 #store everything to a json object
 
 class food:
-    def __init__(self):
-        self.protein = 0
-        self.calories = 0
+    def __init__(self, protein = 0, calories = 0):
+        self.protein = protein
+        self.calories = calories
 
 class day:
-    def __init(self):
-        self.target = 0
-        self.totalCals = 0
+    def __init__(self):
+        self.targetProtein = 0
+        self.totalCalories = 0
+        self.dayName = ""
+        self.totalProtein = 0
 
 #while our target protein isnt hit, we need to continuously take input of foods
 class calorieTracker:
     def __init__(self):
         self.foodMap = {}
+        self.Day = day()
 
-    def trackLoop:
-        while input from user isn't done
+    def trackDay(self):
+        curDay = self.Day
+        curDay.dayName = input("what day of the week is it: ")
+        print("enter q to stop tracker for the day")
+        curCalories = 0
+        curProtein = 0
+        while True:
+            userInput = input("input the food you just ate: ")
+            if userInput == "q":
+                break
+            if userInput.lower() not in self.foodMap:
+                print("this food isnt in the map, please enter the following nutrient for it")
+                foodProtein = int(input("\n protein in grams: "))
+                foodCalories = int(input("\n how many calories: "))
+                self.foodMap[userInput.lower()] = food(foodProtein, foodCalories)
+        
+            curProtein += self.foodMap[userInput.lower()].protein
+            curCalories += self.foodMap[userInput.lower()].calories
+            print(f"current Protein is {curProtein} and our current calories is {curCalories}")
+        curDay.totalProtein = curProtein
+        curDay.totalCalories = curCalories
+        print(curDay)
+        print(f"on {curDay.dayName} we have gotten a total of {curDay.totalProtein}g of protein, and {curDay.totalCalories} calories")
+
+
+"""        while input from user isn't done
             take input
             if its a new food, add it to the map
             track the food
             if we hit the goal, notify the user
-        tell the user his stats
+        tell the user his stats"""
+
+tracker = calorieTracker()
+tracker.trackDay()
