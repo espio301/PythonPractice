63c9ce99b07fb80f81724871f4bcc8bd9f2dc656
Andrew Pearson Caldwell <espio301@Andrews-MacBook-Air.local>
2025-12-24 15:56:20 -0800

    adding get player tests

--- blackjack/tests.py
+++ blackjack/tests.py
@@ -1,5 +1,8 @@
 import logging
 import gameLogic as gl
+import sys
+from io import StringIO
+
 #deck = gl.Deck()
 #card = gl.Card()
 
@@ -77,9 +80,80 @@ class UnitTests:
             print("failed")
             return False
 
+    def deckToStringTest(self):
+        try:
+            deck = gl.Deck()
+            validCards = deck.toString().split("\n")[0]
+            validList = validCards.split(":")
+            if validList[0] != "valid cards" and int(validList[1]) != 52:
+                return False
+            cardList = deck.toString().split("\n")[1].split(", ")
+            cardName = {"ace", "2", "3" , "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"}
+            suitName = {"spades", "clubs", "hearts", "diamonds"}
+           
+            for card in cardList:
+                cardSplit = card.split(" of ")
+                if cardSplit[0] not in cardName or cardSplit[1] not in suitName:
+                    print(f"failed:{cardSplit[0]} , {cardSplit[0] not in cardName} \n{cardSplit[1]} , {cardSplit[1] not in suitName}" )
+                    return False
+            return True
+        except Exception as e:
+            print("failed: ", e)
+            return False
+
+    def getPlayersTest(self):
+        try:
+            sys.stdin = StringIO()
+            table = gl.BlackjackTable()
+            passed = True
+            #standard test
+            sys.stdin.write("andrew\njames\nq\n")
+            sys.stdin.seek(0)
+            table.getPlayers()
+            for player in table.players:
+                print(player.toString())
+            if table.players[0].name != "andrew" and table.players[1].name != "james":
+                print("failed standard test")
+                passed = False
+            table = gl.BlackjackTable()
+            sys.stdin.write("q\n")
+            sys.stdin.seek(15)
+            table.getPlayers()
+            if len(table.players) != 0:
+                print("failed empty player list")
+                passed = False
+            #nonstring input, technically speaking thsi should still pass as technically people can have numbers in their name (elons kid has non english alphabet symbols at least)
+            table = gl.BlackjackTable()
+            sys.stdin.write("123\nq\n")
+            sys.stdin.seek(17)
+            table.getPlayers()
+            if len(table.players) != 1 and table.players[0] != "123":
+                print("failed empty player list")
+                passed = False
+            return passed
+        except Exception as e:
+            print(f"failed with error: {e}")
+            return False
+
+
+            for player in table.players:
+                print(player.toString())
+
+
+        except Exception as e:
+            print(f"failed with exception: {e}")
 
 
 ut = UnitTests()
-print(ut.cardInitTest())
+deck = gl.Deck()
+#sys.stdin.write("myinput")
+#sys.stdin.seek(0)  # rewind, so input() can read
+#text = input()
+#print(f"{text=}")
+
+print(ut.getPlayersTest())
+"""print(ut.cardInitTest())
 print(ut.cardToStringTest())
-print(ut.deckInitTest())
+#print(ut.deckInitTest())
+print(ut.deckToStringTest())
+"""
