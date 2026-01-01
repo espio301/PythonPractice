af242bd3b3da6f2562ed92c7c93dce886d9134d6
Andrew Pearson Caldwell <espio301@Andrews-MacBook-Air.local>
2025-12-31 18:26:26 -0800

    converted on feedback for pre-existing code. need to add recent additions to game_logic still

--- blackjack/tests.py
+++ blackjack/tests.py
@@ -1,251 +1,288 @@
-import logging
-import gameLogic as gl
+import random
+import game_logic as gl
 import sys
 from io import StringIO
+import random
 
-#deck = gl.Deck()
-#card = gl.Card()
-
-#print(deck.toString())
+random.seed(67)
+SUITS_LIST = ["spades", "clubs", "hearts", "diamonds"]
+RANKS_LIST = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
+VALID_ACTIONS = {"s", "h"}
 
 #we should have thorough testing for each function
 class UnitTests:
+    def run_all_tests(self):
+        self.card_to_string_test()
+        self.deck_init_test()
+        self.deck_to_string_test()
+        self.get_players_test()
+        self.calculate_hand_test()
+        self.deal_test()
+        print("passed all tests!")
 
-    def cardInitTest(self):
-        try:
-            card = gl.Card()
-            if card.suit == "" and card.number == 0 and card.numberName == "":
-                return True
-            raise Exception
-        except Exception as error:
-            return False
-
-    def cardToStringTest(self):
-        try:
-            card = gl.Card()
-            if card.toString() == "" + str(card.numberName) + " of " + str(card.suit):
-                return True
-            raise Exception
-        except Exception as error:
-            return False
-
-    #this unit test uses toString, which we probably shouldnt and we can just make a different way to string a card here
-    def deckInitTest(self):
-        try:
-            deck = gl.Deck()
-            if deck.validCards != 52 and len(deck.deck) != 52:
-                return False
-            cardSet = set()
-            #we can actually prove we have 1 of each card without hard coding anything by defining that each card is unique, and then that there are 13 of each suit and 4 of each card! pretty neat if you ask me :)
-            for card in deck.deck:
-                if card.toString() not in cardSet:
-                    cardSet.add(card.toString())
-                else:
-                    return False
-            if len(cardSet) != 52:
-                return False
-            #check suits are correct
-            suitMap = {"spades": 0, "clubs": 0, "hearts": 0 , "diamonds": 0}
-            for card in deck.deck:
-                if card.suit not in suitMap:
-                    return False
-                suitMap[card.suit] += 1
-            for k in suitMap:
-                if suitMap[k] != 13:
-                    return False
-            print(suitMap)
-
-            #checking number Names
-            numberNameMap = {"ace": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "10": 0, "jack": 0, "queen": 0, "king": 0}
-            for card in deck.deck:
-                nn = card.numberName
-                if nn not in numberNameMap:
-                    numberNameMap[nn] = 0
-                numberNameMap[nn] += 1
-            print(numberNameMap)
-            for k in numberNameMap:
-                if numberNameMap[k] != 4:
-                    return False
-
-            numberMap = {"ace" : [1,0], "2" : [2,0], "3" : [3,0], "4": [4,0], "5": [5,0], "6": [6,0], "7": [7,0], "8": [8,0], "9": [9,0], "10": [10,0], "jack": [10,0], "queen": [10,0], "king": [10,0]}
-            for card in deck.deck:
-                if card.numberName in numberMap and numberMap[card.numberName][0] == card.number:
-                     numberMap[card.numberName][1] += 1
-            print(numberMap)
-            for k in numberMap:
-                if numberMap[k][1] != 4:
-                    return False
+    def card_init_test(self):
+        card = gl.Card()
+        if card.suit == "" and card.number == 0 and card.numberName == "":
             return True
-        except Exception as e:
-            print("failed")
-            return False
+        raise Exception
 
-    def deckToStringTest(self):
-        try:
-            deck = gl.Deck()
-            validCards = deck.toString().split("\n")[0]
-            validList = validCards.split(":")
-            if validList[0] != "valid cards" and int(validList[1]) != 52:
+    def to_string_face_card_test(self):
+        face_card_ranks = ["ace", "jack", "queen", "king"]
+        face_card_vals = [1,10,10,10]
+        for i in range(0,4):
+            test_card = gl.Card(face_card_ranks[i], face_card_vals[i], SUITS_LIST[i])
+            assert test_card.to_string() == f"{face_card_ranks[i]} of {SUITS_LIST[i]}"
+        return True
+
+    def to_string_number_card_test(self):
+        card_ranks = [str(x) for x in range(2,11)]
+        card_vals = [x for x in range(2,11)]
+        for i in range(0,9):
+            test_card = gl.Card(card_ranks[i], card_vals[i], SUITS_LIST[i%4])
+            assert test_card.to_string() == f"{card_ranks[i]} of {SUITS_LIST[i%4]}"
+        return True
+
+    def card_to_string_test(self):
+        self.to_string_face_card_test()
+        self.to_string_number_card_test()
+        print("successfully passed all to_string tests")
+
+
+    def check_card_uniqueness(self, deck):
+        card_set = set()
+        for card in deck:
+            if card.to_string() not in card_set:
+                card_set.add(card.to_string())
+            else:
                 return False
-            cardList = deck.toString().split("\n")[1].split(", ")
-            cardName = {"ace", "2", "3" , "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"}
-            suitName = {"spades", "clubs", "hearts", "diamonds"}
-           
-            for card in cardList:
-                cardSplit = card.split(" of ")
-                if cardSplit[0] not in cardName or cardSplit[1] not in suitName:
-                    print(f"failed:{cardSplit[0]} , {cardSplit[0] not in cardName} \n{cardSplit[1]} , {cardSplit[1] not in suitName}" )
-                    return False
-            return True
-        except Exception as e:
-            print("failed: ", e)
+
+    def check_card_count(self, deck):
+        counter = 0
+        for card in deck:
+            counter += 1
+        return counter == 52
+
+    def check_suit_count(self, deck):
+        suitMap = {"spades": 0, "clubs": 0, "hearts": 0 , "diamonds": 0}
+        for card in deck:
+            if card.suit not in suitMap:
+                return False
+            suitMap[card.suit] += 1
+        for k in suitMap:
+            if suitMap[k] != 13:
+                return False
+        return True
+
+    def check_rank_count(self, deck):
+        rank_map = {"ace": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "10": 0, "jack": 0, "queen": 0, "king": 0}
+        for card in deck:
+            rank = card.rank
+            if rank not in rank_map:
+                rank_map[rank] = 0
+            rank_map[rank] += 1
+
+        for k in rank_map:
+            if rank_map[k] != 4:
+                return False
+        return True
+
+    #note to self: would be more readable if I just had two different maps here with distinguished names
+    def check_value_correctness(self, deck):
+        #value in the form of [rank's correct value, amount of times we see the value]
+        number_map = {"ace" : [1,0], "2" : [2,0], "3" : [3,0], "4": [4,0], "5": [5,0], "6": [6,0], "7": [7,0], "8": [8,0], "9": [9,0], "10": [10,0], "jack": [10,0], "queen": [10,0], "king": [10,0]}
+        for card in deck:
+            if card.rank in number_map and number_map[card.rank][0] == card.number:
+                    number_map[card.rank][1] += 1
+        for k in number_map:
+            if number_map[k][1] != 4:
+                return False
+        return True
+
+
+    def deck_init_test(self):
+        deck = gl.Deck().deck #definitely should rename this field
+        #we can actually prove we have 1 of each card without hard coding anything by defining that each card is unique, and then that there are 13 of each suit and 4 of each card! pretty neat if you ask me :)
+        assert self.check_card_count(deck) == True
+        assert self.check_card_uniqueness(deck) == True
+        #check suits are correct
+        assert self.check_suit_count(deck) == True
+        assert self.check_rank_count(deck) == True
+        assert self.check_value_correctness(deck)
+
+    def get_expected_string(self, deck : list[gl.Card]):
+        prefix = f"cards in deck: {len(deck)}\n"
+        card_strings = []
+        for card in deck:
+            card_strings.append(card.to_string())
+        return prefix + ", ".join(card_strings)
+
+    def deck_to_string_test(self):
+        deck = gl.Deck().deck
+        expected = self.get_expected_deck_string(deck)
+
+        assert expected == deck.to_string()
+        
+
+    def add_two_player_test(self):
+        sys.stdin.write("andrew\njames\nq\n")
+        sys.stdin.seek(0)
+        table.getPlayers()
+        for player in table.players:
+            print(player.toString())
+        if table.players[0].name != "andrew" and table.players[1].name != "james":
+            print("failed standard test")
             return False
+        return True
+
+    def add_no_player_test(self):
+        table = gl.BlackjackTable()
+        sys.stdin.write("q\n")
+        sys.stdin.seek(15)
+        table.getPlayers()
+        if len(table.players) != 0:
+            print("failed empty player list")
+            return False
+        return True
+
+    def add_numerical_name(self):
+        #expected value atm is for it to be ok to have numbers in names
+        table = gl.BlackjackTable()
+        sys.stdin.write("123\nq\n")
+        sys.stdin.seek(17)
+        table.getPlayers()
+        if len(table.players) != 1 and table.players[0] != "123":
+            print("failed empty player list")
+            return False
+        return True
 
     #need to implement if there are no players
-    def getPlayersTest(self):
-        try:
-            sys.stdin = StringIO()
-            table = gl.BlackjackTable()
-            passed = True
-            #standard test
-            sys.stdin.write("andrew\njames\nq\n")
-            sys.stdin.seek(0)
-            table.getPlayers()
-            for player in table.players:
-                print(player.toString())
-            if table.players[0].name != "andrew" and table.players[1].name != "james":
-                print("failed standard test")
-                passed = False
-            table = gl.BlackjackTable()
-            sys.stdin.write("q\n")
-            sys.stdin.seek(15)
-            table.getPlayers()
-            if len(table.players) != 0:
-                print("failed empty player list")
-                passed = False
-            #nonstring input, technically speaking this should still pass as technically people can have numbers in their name (elons kid has non english alphabet symbols at least)
-            table = gl.BlackjackTable()
-            sys.stdin.write("123\nq\n")
-            sys.stdin.seek(17)
-            table.getPlayers()
-            if len(table.players) != 1 and table.players[0] != "123":
-                print("failed empty player list")
-                passed = False
-            return passed
-        except Exception as e:
-            print(f"failed with error: {e}")
-            return False
+    def get_players_test(self):
+        sys.stdin = StringIO()
+        table = gl.BlackjackTable()
+        #standard test
+        assert add_two_player_test()
+        assert add_no_player_test()
+        #nonstring input, technically speaking this should still pass as technically people can have numbers in their name (elons kid has non english alphabet symbols at least)
+        assert add_numerical_name()
+        return True
+    
+    def get_deck_no_aces(self):
+        deck = gl.Deck().deck
+        no_ace_deck = []
+        for card in deck:
+            if deck.rank == "ace":
+                continue
+            no_ace_deck.append(card)
+        return no_ace_deck
+
+    def get_deck_of_aces(self):
+        aces = []
+        for suit in SUITS_LIST:
+            aces.append(Card("ace", 1, suit))
+        return aces
+
+    def calculate_no_ace_hand():
+        no_ace_deck = self.get_deck_no_aces()
+        for card in deck:
+            if deck.rank == "ace":
+                continue
+            no_ace_deck.append(card)
         
-        #mainly need to assure aces logic works appropriately
-    def calculateHandTest(self):
+        #hard coding 48 as if the length isn't 48 we have a different issue in deck initialization
+        hand = []
+        hand_value = 0
+        while len(no_ace_deck) > 0:
+            #we're going to get a random card from the no ace deck
+            index = random.randrange(0,len(no_ace_deck))
+            card = no_ace_deck[index]
+            no_ace_deck.remove[index]
+            #add it to the hand
+            hand.append(card)
+
+            #check if calculation is correct
+            hand_value += card.value
+            gl.Player("andrew", hand)
+            assert hand_value == gl.Player()
+
+        print("passed")
+            
+            
+    #was stuck on this a bit
+    def calculate_with_ace_hand(self):
+        non_aces = self.get_deck_no_aces()
+        aces = self.get_deck_of_aces()
+        deck = gl.Deck().deck
+        #create a hand with 1 ace and hand value is less than ten
+        hand = []
+        hand_value = 11
+        non_ace_value = 0
+        hand.append(aces[random.randrange(0,4)])
+
+        while len(hand) < 20:
+            index = random.randrange(0,len(deck))
+            card = deck[index]
+            if card.rank == "ace" and card.suit == hand[0].suit:
+                deck.remove(index)
+                continue
+            hand.append(card)
+            non_ace_value += card.value
+            if non_ace_value > 10:
+                hand_value -= 10
+            assert Player("andrew", hand) == hand_value
+
+    def calculate_hand_test(self):
     #check standard test
-        passed = True
-        c1 = gl.Card("2",2,"clubs")
-        c2 = gl.Card("3",3,"clubs")
-        p1 = gl.Player("andrew", [c1,c2])
-        if p1.calculateHand() != 5:
-            print(f"failed standard test, hand val was {p1.calculateHand()} when cards were {c1.numberName} and {c2.numberName}")
-            passed = False
-
-        c1 = gl.Card("ace",1,"clubs")
-        c2 = gl.Card("ace",1,"spade")
-        p1 = gl.Player("andrew", [c1,c2])
-        if p1.calculateHand() != 12:
-            print(f"failed double aces test, hand val was {p1.calculateHand()} when cards were {c1.numberName} and {c2.numberName}")
-            passed = False
-
-        c1 = gl.Card("jack",10,"clubs")
-        c2 = gl.Card("ace",1,"spade")
-        c3 = gl.Card("5",5,"spade")
-
-        p1 = gl.Player("andrew", [c1,c2,c3])
-        if p1.calculateHand() != 16:
-            print(f"failed 3 cards with ace thats less than 11 test, hand val was {p1.calculateHand()} when cards were {c1.numberName}, {c2.numberName}, and {c3.numberName}")
-            passed = False
-
-        c1 = gl.Card("jack",10,"clubs")
-        c2 = gl.Card("9",9,"spade")
-        c3 = gl.Card("10",10,"spade")
-        p1 = gl.Player("andrew", [c1,c2,c3])
-        if p1.calculateHand() != 29:
-            print(f"bust test, hand val was {p1.calculateHand()} when cards were {c1.numberName}, {c2.numberName}, and {c3.numberName}")
-            passed = False
-
-        c1 = gl.Card("ace",1,"clubs")
-        c2 = gl.Card("ace",1,"spade")
-        c3 = gl.Card("10",10,"spade")
-        p1 = gl.Player("andrew", [c1,c2,c3])
-        if p1.calculateHand() != 12:
-            print(f"failed multiple aces, neither 11 test, hand val was {p1.calculateHand()} when cards were {c1.numberName}, {c2.numberName}, and {c3.numberName}")
-            passed = False
+        self.calculate_no_ace_hand()
+        self.calculate_with_ace_hand()
         return passed
-        #can assume there exists players as we test for this beforehand
-    def getWinnersTest(self):
-        try:
-            #standard test 
-            table = gl.BlackjackTable()
-            passed = True
-            c11 = gl.Card("9", 9, "spades")
-            c12 = gl.Card("ace", 1, "clubs")
-            c21 = gl.Card("8", 8, "spades")
-            c22 = gl.Card("10", 10, "spades")
-            p1 = gl.Player("andrew", [c11,c12])
-            p2 = gl.Player("james", [c21,c22])
-            if table.getWinners([p1,p2]) != ["andrew"]:
-                print(f"failed standard test between {p1.hand.toString()} and {p2.hand.toString()}")
-                passed = False
 
-            #test tie
-            c11 = gl.Card("9", 9, "spades")
-            c12 = gl.Card("ace", 1, "clubs")
-            c21 = gl.Card("8",10, "diamonds")
-            c22 = gl.Card("10", 10, "spades")
-            p1 = gl.Player("andrew", [c11,c12])
-            p2 = gl.Player("james", [c21,c22])
-            if table.getWinners([p1,p2]) != ["andrew", "james"]:
-                print(f"failed tie between {p1.hand.toString()} and {p2.hand.toString()}")
-                passed = False
 
-            #test for a tie then a winner after
-            c31 = gl.Card("10",10,"spade")
-            c32 = gl.Card("ace", 1, "clubs")
-            p3 = gl.Player("shomik", [c31, c32])
-            if table.getWinners([p1,p2,p3]) != ["shomik"]:
-                print(f"failed tie at first, then a winner is apparent: {p1.hand.toString()}, {p2.hand.toString()}, then {p3.hand.toString()}")
-                passed = False 
+    def empty_winners_test(self):
+        table = gl.BlackjackTable()
+        assert table.get_winners() == []
 
-            #test empty
-            if table.getWinners([]) != []:
-                print("failed empty player list")
-                passed = False
-            return passed
-        except Exception as e:
-            print(f"failed with error: {e}")
-            return False
-        #test for one player who bust
-        #standard test between two players
-        #standard test between 3 players
+    def multiplayer_winners_test(self):
+        num_players = random.randrange(1,11)
+        table = gl.BlackjackTable()
+        table.players = [Player("test player") for x in range(1,num_players)]
+        for player in table.players:
+            table.starting_deal(player)
+        winners = []
+        max_hand = 0
+        for player in table.players:
+            if player.calculate_hand() == max_hand:
+                winners.append(player.name)
+            if player.calculate_hand() > max_hand:
+                max_hand = player.calculate_hand()
+                winners = [player]
+        assert winners == table.get_winners()
+
+    def get_winners_test(self):
+        self.empty_winners_test()
+        self.multiplayer_winners_test()
+
+
 
     #this one's fairly important, need to check that we don't deal repeated card ever.
     #we're going to deal all 52 cards and make sure each card dealt is unique
-    def dealTest(self):
-        cardSet = set()
+    def deal_test(self):
         table = gl.BlackjackTable()
-        p1 = gl.Player()
-        while table.tableDeck.validCards > 0:
-            card = table.dealCard(p1)
-            assert card.toString() == p1.hand[-1].toString()
-            assert card.toString() not in cardSet
-            cardSet.add(card.toString)
+        p1 = gl.Player("andrew")
+        while len(table.tableDeck) > 0:
+            card_to_deal = deck[-1]
+            card = table.deal_card(p1)
+            assert card_to_deal == card
+            assert card == p1.hand[-1]
         return True
 
+
+    #last test is for the gameloop, which is essentially an integration test
 ut = UnitTests()
-deck = gl.Deck()
 #sys.stdin.write("myinput")
 #sys.stdin.seek(0)  # rewind, so input() can read
 #text = input()
 #print(f"{text=}")
-
+ut.run_all_tests()
 print(ut.dealTest())
 """print(ut.cardInitTest())
 print(ut.cardToStringTest())
