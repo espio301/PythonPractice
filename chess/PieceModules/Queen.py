23c95df3a6183da66ea39c34b56bb577655e39ee
Andrew Pearson Caldwell <espio301@Andrews-MacBook-Air.local>
2026-01-19 03:51:09 -0800

    looks like the last bug is just in is_king_of_color_moveable

--- chess/PieceModules/Queen.py
+++ chess/PieceModules/Queen.py
@@ -20,11 +20,3 @@ class Queen(Piece):
     def get_coords(self):
         return super().get_coords()
 
-    def set_coords(self, coords):
-        super().set_coords(coords)
-
-    def to_string(self):
-        return super().to_string()
-
-    def get_color(self):
-        return super().get_color()
