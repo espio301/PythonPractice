8f79c8263c31504a5edf4061d2c19573ca25a957
Andrew Pearson Caldwell <espio301@Andrews-MacBook-Air.local>
2026-01-19 19:38:37 -0800

    I separated a bunch of functions I'm going to move to a helper class when I have more time

--- chess/TestModules/ChessBoardTests.py
+++ chess/TestModules/ChessBoardTests.py
@@ -6,6 +6,7 @@ from PieceModules.Queen import Queen
 from PieceModules.King import King
 from PieceModules.Pawn import Pawn
 from io import StringIO
+from contextlib import redirect_stdout
 import sys
 
 class ChessBoardTests:
@@ -22,6 +23,8 @@ class ChessBoardTests:
         self.convert_input_to_coords_test()
         self.get_sanitized_coords_test()
         self.get_user_piece_to_move_test()
+        self.is_moving_non_king_in_check_test()
+        self.is_valid_movement_test()
         print("finished with ChessBoard tests")
 
     def assert_first_last_row_correctness(self):
@@ -32,7 +35,7 @@ class ChessBoardTests:
             white_piece = chessboard[7][i]
             assert isinstance(black_piece, row_pieces[i]) and isinstance(white_piece, row_pieces[i]) and black_piece.get_color() == "black" and white_piece.get_color()
 
-
+#TODO eventually I'm going to refactor this into TestHelper. After I make the chess letter/coord converter I'll make a board creator helper with all of these. given string of chess moves it will create a board of that state
     def create_board_white_in_check(self):
         chessboard = ChessBoard()
         chessboard.board = [[Rook("black", [0,0]),0,0,0,0,0,0,0],\
@@ -44,7 +47,36 @@ class ChessBoardTests:
                             [0,0,0,0,0,0,0,0],\
                             [King("white", [7,0]),0,0,0,0,0,0,Rook("black",[0,0])]]
         return chessboard
-                            
+
+    def create_board_pawn_can_move_diagonally(self):
+        chessboard = ChessBoard()
+        chessboard.board = [[0,0,0,0,0,0,0,0],\
+                            [0,0,0,0,0,0,0,0],\
+                            [0,0,0,0,0,0,0,0],\
+                            [0,Pawn("black", [3,1]),0,0,0,0,0,0],\
+                            [Pawn("white",[4,0]),0,0,0,0,0,0,0],\
+                            [0,0,0,0,0,0,0,0],\
+                            [0,0,0,0,0,0,0,0],\
+                            [0,0,0,0,0,0,0,0]]
+        return chessboard
+
+    def write_seek_new_stdin(self, write_string):
+        sys.stdin = StringIO()
+        sys.stdin.write(write_string)
+        sys.stdin.seek(0)
+
+    def silent_run(self, func, func_in = None):
+        output = StringIO()
+        with redirect_stdout(output):
+            if func_in != None:
+                func(func_in)
+            func()
+        return output
+
+
+
+
+#everything above is to be put in a helper class
 
     def assert_pawn_rows_correctness(self):
         chessboard = ChessBoard().board
@@ -98,16 +130,11 @@ wr | wk | wb | wQ | wK | wb | wk | wr"""
         self.write_seek_new_stdin(f"{unsanitizable_string}\n6,7\n")
         assert ChessBoard().get_sanitized_coords() == [6,7]
 
-    def write_seek_new_stdin(self, write_string):
-        sys.stdin = StringIO()
-        sys.stdin.write(write_string)
-        sys.stdin.seek(0)
-
     def get_user_piece_to_move_test(self):
-        self.check_correct_piece_to_move()
-        self.check_user_moves_not_a_piece()
-        self.check_user_moves_other_player_piece()
-        self.check_user_tries_moving_in_check()
+        self.silent_run(self.check_correct_piece_to_move)
+        self.silent_run(self.check_user_moves_not_a_piece)
+        self.silent_run(self.check_user_moves_other_player_piece)
+        self.silent_run(self.check_user_tries_moving_in_check)
         
     def check_correct_piece_to_move(self):
         self.write_seek_new_stdin("0,0\n")
@@ -130,3 +157,32 @@ wr | wk | wb | wQ | wK | wb | wk | wr"""
         chessboard = self.create_board_white_in_check()
         self.write_seek_new_stdin("7,7\n7,0\n")
         assert chessboard.get_user_piece_to_move("white") == [7,0]
+
+    def is_moving_non_king_in_check_test(self):
+        self.silent_run(self.check_user_tries_moving_in_check) #leaving this here for consistency of my testing and in future it may want to be done more thoroughly (feel free to tell me to delete this though, I'm only leaving this comment in case a coach wants to course correct this thought process)
+
+    def is_valid_movement_test(self):
+        self.test_valid_movements()
+        self.test_invalid_movements()
+        self.test_banned_pawn_movement()
+
+    def test_valid_movements(self):
+        chessboard = ChessBoard()
+        assert chessboard.is_valid_movement([6,0], [5,0]) and chessboard.is_valid_movement([7,1],[5,2])
+
+    def test_invalid_movements(self):
+        chessboard = ChessBoard()
+        assert chessboard.is_valid_movement([7,0],[6,1]) == False and chessboard.is_valid_movement([7,0],[5,2]) == False
+    
+    def test_banned_pawn_movement(self):
+        self.test_valid_diag_pawn_movement()
+        self.test_invalid_diag_pawn_movement()
+
+    def test_valid_diag_pawn_movement(self):
+        chessboard = self.create_board_pawn_can_move_diagonally()
+        assert chessboard.is_valid_movement([4,0],[3,1])
+
+    def test_invalid_diag_pawn_movement(self):
+        assert ChessBoard().is_valid_movement([6,0],[5,1]) == False
+
+
